029f39c678404521f8b1e0cbc7de4265cde436d1
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2025-01-01 21:47:17 +0700

    Fix connection string

--- Program.cs
+++ Program.cs
@@ -3,8 +3,21 @@ using CommentApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var mongoConnectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING");
+var mongoDatabaseName = Environment.GetEnvironmentVariable("MONGODB_DATABASE_NAME");
+
+if (string.IsNullOrEmpty(mongoConnectionString) || string.IsNullOrEmpty(mongoDatabaseName))
+{
+    throw new InvalidOperationException("MongoDB connection string or database name is missing.");
+}
+
 // Add services to the container.
-builder.Services.Configure<CommentDatabaseSettings>(builder.Configuration.GetSection("CommentDatabase"));
+builder.Services.Configure<CommentDatabaseSettings>(options =>
+{
+    options.ConnectionString = mongoConnectionString;
+    options.DatabaseName = mongoDatabaseName;
+});
+
 builder.Services.AddSingleton<CommentsService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
