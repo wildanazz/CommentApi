81cedd9e5b8b04c83de77e8d68af981ad07cd635
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2025-01-01 21:34:14 +0700

    Add validation to Comment description

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,6 +1,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommentApi.Models;
 
@@ -14,6 +15,8 @@ public class Comment
 
     [BsonElement("Comment")]
     [JsonPropertyName("Comment")]
+    [Required]
+    [StringLength(500, ErrorMessage = "Comment cannot be longer than 500 characters.")]
     public string Description { get; set; } = null!;
 
     [BsonElement("CreatedAt")]
